ddf1cb6e21aa1bb1314e3ab5a2c437a2deeab0e5
Đinh Quang Tuấn <quangtuan496@gmail.com>
2024-04-22 11:43:37 +0700

    react-type-animation + typing welcome

--- HTML-JS/portfolio/src/pages/Home/Home.jsx
+++ HTML-JS/portfolio/src/pages/Home/Home.jsx
@@ -5,12 +5,16 @@ import IMAGES from "../../assets/imgs";
 import styles from "../../components/styles/Home.module.scss"
 import classNames from "classnames/bind";
 
+// https://react-type-animation.netlify.app
+// https://github.com/maxeth/react-type-animation
+import { TypeAnimation } from 'react-type-animation';
+
 const cx = classNames.bind(styles);
 
 export default function Home() {
   return (
     <div>
-      <section className={cx(["parallax", "overflow-hidden z-0"])}>
+      <section className={cx(["parallax", "relative overflow-hidden z-0"])}>
         <img src={IMAGES.night_skyy} alt="night-sky" className={cx(["sky", ""])} />
         <img src={IMAGES.stars} alt="stars1" className={cx(["stars1", "left-[-50%]"])} />
         <img src={IMAGES.stars} alt="stars2" className={cx(["stars2", "left=[50%]"])} />
@@ -19,6 +23,18 @@ export default function Home() {
         <img src={IMAGES.flying_bird2} alt="bird2" className={cx(["bird2", ""])} />
         <img src={IMAGES.flying_bird2} alt="bird3" className={cx(["bird3", ""])} />
         <img src={IMAGES.night_sea} alt="night-sea" className={cx(["sea", ""])} />
+        <TypeAnimation 
+          className={cx(["lblWelcome", "absolute z-[1] top-[50%] left-[50%] text-6xl"])} 
+          sequence={['Welcome!', 0]}
+          speed={10}
+          repeat={0}
+          cursor={false}
+        />
+      </section>
+      <section className={cx(["portfolio-container", "relative mt-[47%] overflow-hidden pb-[2%]"])}>
+        <div>
+          
+        </div>
       </section>
     </div>
   )
