10d3cf083d8a7e8307d810edc6601a6f663d475c
Đinh Quang Tuấn <quangtuan496@gmail.com>
2024-08-20 13:02:37 +0700

    Update index.js

--- HTML-JS/portfolio/server/index.js
+++ HTML-JS/portfolio/server/index.js
@@ -23,7 +23,7 @@ app.use(
     credentials: true,
     origin:
       "http://localhost:5173" ||
-      "https://portfolio-dinh-quang-tuan-server.onrender.com",
+      "https://portfolio-dinh-quang-tuan-server.onrender.com//api",
   })
 );
 
