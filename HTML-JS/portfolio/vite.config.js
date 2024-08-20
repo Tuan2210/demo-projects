0fc8223df97de3932354cf7864550c1d9668a816
Đinh Quang Tuấn <quangtuan496@gmail.com>
2024-08-20 12:29:12 +0700

    Update vite.config.js

--- HTML-JS/portfolio/vite.config.js
+++ HTML-JS/portfolio/vite.config.js
@@ -53,7 +53,7 @@ export default defineConfig({
     proxy: {
       "/api": {
         target:
-          "http://localhost:3000/api" ||
+          // "http://localhost:3000/api" ||
           "https://portfolio-dinh-quang-tuan-server.onrender.com/api",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
