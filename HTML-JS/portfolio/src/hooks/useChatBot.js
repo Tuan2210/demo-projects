ec28c858f30ed5a631509166033d1a2598a64e53
Đinh Quang Tuấn <quangtuan496@gmail.com>
2025-02-22 11:06:37 +0700

    upgrade Gemini 1.5 Flash to 2.5 Flash API

--- HTML-JS/portfolio/src/hooks/useChatBot.js
+++ HTML-JS/portfolio/src/hooks/useChatBot.js
@@ -1,24 +1,29 @@
-import axios from "axios";
-
-import { API_GEMINI_URL } from "@constants/url";
-import { GEMINI_KEY } from "@constants/envUrl";
+import { fetchGeminiMsg } from "@services/apiRequests";
+// import axios from "axios";
+// import { API_GEMINI_URL } from "@constants/url";
+// import { GEMINI_KEY } from "@constants/envUrl";
 
 export default async function useChatBot(question, setIsReplying) {
   let msg = {};
   try {
-    const response = await axios({
-      url: `${API_GEMINI_URL}?key=${GEMINI_KEY}`,
-      method: "post",
-      data: {
-        contents: [{ parts: [{ text: question }] }],
-      },
-      // timeout: 2000
-    });
+    // Gemini 1.5 Flash
+    // const response = await axios({
+    //   url: `${API_GEMINI_URL}?key=${GEMINI_KEY}`,
+    //   method: "post",
+    //   data: {
+    //     contents: [{ parts: [{ text: question }] }],
+    //   },
+    //   // timeout: 2000
+    // });
+
+    // Gemini 2.0 Flash
+    const response = await fetchGeminiMsg({ prompt: question });
 
     msg = {
       position: "left",
       type: "text",
-      text: response["data"]["candidates"][0]["content"]["parts"][0]["text"],
+      // text: response["data"]["candidates"][0]["content"]["parts"][0]["text"], // Gemini 1.5 Flash
+      text: response.data, // Gemini 2.0 Flash
     };
 
     // scroll to bottom  msg-list
--- HTML-JS/portfolio/src/services/apiRequests.js
+++ HTML-JS/portfolio/src/services/apiRequests.js
@@ -19,3 +19,12 @@ export const addPageVisit = async (pageVisitObj) => {
     console.error("Error adding page-visit", error);
   }
 };
+
+export const fetchGeminiMsg = async (prompt) => {
+  try {
+    const res = await axios.post(`${API_URL}/api/chatBot/Gemini2.0Flash`, prompt, { timeout: 5000 });
+    return res.data;
+  } catch (error) {
+    console.error("Error fetching Gemini 2.0 Flash", error);
+  }
+}
